65e79aab28089f4110e433da9c77e6468de87be5
Leonel <onixdelgado95@gmail.com>
2026-02-24 02:26:13 -0300

    agregar boton de actualizar v6

--- assets/js/pwa-update.js
+++ assets/js/pwa-update.js
@@ -1,7 +1,7 @@
 // assets/js/pwa-update.js
 
 // 1. DEFINE TU VERSIÓN AQUÍ
-const APP_VERSION = '0.2.6'; 
+const APP_VERSION = '0.2.7'; 
 
 document.addEventListener('DOMContentLoaded', () => {
     
