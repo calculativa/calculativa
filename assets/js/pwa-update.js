72bf8217c0dc67939eafb4788d7b88de31225a20
Leonel <onixdelgado95@gmail.com>
2026-02-24 18:16:49 -0300

    agregar efecto blur en menus v2

--- assets/js/pwa-update.js
+++ assets/js/pwa-update.js
@@ -1,7 +1,7 @@
 // assets/js/pwa-update.js
 
 // 1. DEFINE TU VERSIÓN AQUÍ
-const APP_VERSION = '0.2.8'; 
+const APP_VERSION = '0.2.9'; 
 
 document.addEventListener('DOMContentLoaded', () => {
     
