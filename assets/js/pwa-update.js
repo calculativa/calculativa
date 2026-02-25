4e3838eec0faf0afbc6ccd86d5748024a8717d65
Leonel <onixdelgado95@gmail.com>
2026-02-25 19:11:31 -0300

    agregado de visualizacion de regimen v2

--- assets/js/pwa-update.js
+++ assets/js/pwa-update.js
@@ -1,7 +1,7 @@
 // assets/js/pwa-update.js
 
 // 1. DEFINE TU VERSIÓN AQUÍ
-const APP_VERSION = '0.2.15'; 
+const APP_VERSION = '0.2.16'; 
 
 document.addEventListener('DOMContentLoaded', () => {
     
