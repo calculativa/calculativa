b65501ce72d582a4a9b8e53925982b32b9be5a2e
Leonel <onixdelgado95@gmail.com>
2026-02-25 03:32:10 -0300

    funciones de botones de materias v2

--- assets/js/pwa-update.js
+++ assets/js/pwa-update.js
@@ -1,7 +1,7 @@
 // assets/js/pwa-update.js
 
 // 1. DEFINE TU VERSIÓN AQUÍ
-const APP_VERSION = '0.2.13'; 
+const APP_VERSION = '0.2.15'; 
 
 document.addEventListener('DOMContentLoaded', () => {
     
