b9c704a002c6a7779df04662839d1c9a56b4ad59
Leonel <onixdelgado95@gmail.com>
2026-02-28 05:26:38 -0300

    Logica de Calculadora Iniciada v2

--- assets/js/pwa-update.js
+++ assets/js/pwa-update.js
@@ -1,7 +1,7 @@
 // assets/js/pwa-update.js
 
 // 1. DEFINE TU VERSIÓN AQUÍ
-const APP_VERSION = '0.2.19'; 
+const APP_VERSION = '0.3.1'; 
 
 document.addEventListener('DOMContentLoaded', () => {
     
